63dde14bdddc6ce6d5f0dc42911de7424f2f2c3b
KingRekterThe2nd <152184083+KingRekterThe2nd@users.noreply.github.com>
2023-11-27 15:10:24 +0100

    Update generatePriceAction.py

--- generatePriceAction.py
+++ generatePriceAction.py
@@ -12,6 +12,17 @@ def calculate_sma(prices, window):
             sma_values.append(sma)
     return sma_values
 
+def calculate_ema(prices, window):
+    ema_values = []
+    multiplier = 2 / (window + 1)
+    ema = sum(prices[:window]) / window
+    ema_values.extend([None] * (window - 1))  # Adding None values for initial indices
+    ema_values.append(ema)
+    for i in range(window, len(prices)):
+        ema = (prices[i] - ema) * multiplier + ema
+        ema_values.append(ema)
+    return ema_values
+
 def calculate_rsi(prices, window=14):
     deltas = [prices[i + 1] - prices[i] for i in range(len(prices) - 1)]
     gains = [delta if delta > 0 else 0 for delta in deltas]
@@ -48,14 +59,15 @@ def generate_crypto_price_action(days, volatility=0.5, drift=0.1, seed=None):
         prices.append(price)
     
     sma_20 = calculate_sma(prices, 20)
+    ema_50 = calculate_ema(prices, 50)
     rsi_14 = calculate_rsi(prices)
     
-    return prices, initial_price, sma_20, rsi_14
+    return prices, initial_price, sma_20, ema_50, rsi_14
 
 # Generate price data for a crypto asset
 num_days = 365  # Number of days
 seed_value = 42  # Seed for replication
-price_data, initial_price, sma_20, rsi_14 = generate_crypto_price_action(num_days, volatility=0.8, drift=0.15, seed=seed_value)
+price_data, initial_price, sma_20, ema_50, rsi_14 = generate_crypto_price_action(num_days, volatility=0.8, drift=0.15, seed=seed_value)
 
 # Save price data and indicators to a JSON file along with initial price and seed for replication
 data_to_save = {
@@ -63,6 +75,7 @@ data_to_save = {
     'initial_price': initial_price,
     'prices': price_data,
     'sma_20': sma_20,
+    'ema_50': ema_50, 
     'rsi_14': rsi_14
 }
 
@@ -71,15 +84,16 @@ with open(f'crypto_price_data_{seed_value}.json', 'w') as json_file:
 
 # Plotting the price action for a crypto asset along with indicators
 fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8), gridspec_kw={'height_ratios': [3, 1]})
-ax1.plot(range(0,len(price_data)), price_data, label='Price')
-ax1.plot(range(0,len(price_data)), sma_20, label='SMA (20)')
+ax1.plot(range(0,len(price_data)), price_data, label='Price',color='black')
+ax1.plot(range(0,len(price_data)), sma_20, label='SMA (20)', color='orange')
+ax1.plot(range(0,len(price_data)), ema_50, label='EMA (50)',color='blue')
 ax1.set_title('Simulated Crypto Asset Price Action with SMA (20)')
 ax1.set_ylabel('Price')
 ax1.legend()
 ax1.grid(axis='y')  # Horizontal grid lines only
 ax1.xaxis.grid(False)  # Disable vertical grid lines
 
-ax2.plot(range(0,len(price_data)), rsi_14, color='orange', label='RSI (14)')
+ax2.plot(range(0,len(price_data)), rsi_14, label='RSI (14)',color='black')
 ax2.set_title('RSI (14)')
 ax2.set_xlabel('Days')
 ax2.set_ylabel('RSI Value')
